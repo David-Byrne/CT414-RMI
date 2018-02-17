83c12db627d520e949fbdf6f30188618b275d5a1
kettorina <kettorina@users.noreply.github.com>
2018-02-17 18:33:50 +0000

    Solved error with invalid question

--- ct414/AssessmentGUI.java
+++ ct414/AssessmentGUI.java
@@ -5,40 +5,128 @@
 package ct414;
 
 import java.awt.*;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import javax.swing.*;
 import javax.swing.GroupLayout;
 
 /**
  * @author Jekaterina Zenkevica
  */
-public class Assessmenr extends JFrame {
-    public Assessmenr() {
+public class AssessmentGUI extends JFrame {
+
+    Assessment assessment;
+    ExamServer examServer;
+    int token;
+    int ID_NUMBER;
+    Question selectedQuestion;
+
+    public AssessmentGUI(ExamServer server, int token, int username, Assessment assessment) {
+        this.examServer = server;
+        this.assessment = assessment;
+        this.token = token;
+        this.ID_NUMBER = username;
         initComponents();
+    }
+
+    class ItemChangeListener implements ItemListener {
+        @Override
+        public void itemStateChanged(ItemEvent event) {
+            if (event.getStateChange() == ItemEvent.SELECTED) {
+                //as we are selecting from the combo box, the user cannot type in invalid question number
+                try {
+                    selectedQuestion = assessment.getQuestion(comboBox1.getSelectedIndex() + 1);
+                    System.out.println(comboBox1.getSelectedIndex() + 1);
+                    QuestionValue.setText(selectedQuestion.getQuestionDetail());
+                    QuestionValue.setVisible(true);
+                }catch (InvalidQuestionNumber invalidQuestionNumber){
+                    System.err.println("Invalid Question Number");
+                    invalidQuestionNumber.printStackTrace();
+                }
+            }
+        }
     }
 
     private void initComponents() {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
         // Generated using JFormDesigner Evaluation license - Jekaterina Zenkevica
+        label1 = new JLabel();
+        label2 = new JLabel();
+        comboBox1 = new JComboBox();
+        Select = new JLabel();
+        QuestionValue = new JLabel();
 
         //======== this ========
+        setTitle(assessment.getAssessmentCode());
         Container contentPane = getContentPane();
+
+        //---- label1 ----
+        label1.setText("Due: " + assessment.getClosingDate());
+
+        //---- label2 ----
+        label2.setText("Available questions: " + assessment.getQuestions().length);
+
+        //---- Select ----
+        Select.setText("Select from: ");
+
+        //---- QuestionValue ----
+        QuestionValue.setText("text");
+        QuestionValue.setVisible(false);
+
+        for (int i = 1; i<= assessment.getQuestions().length; i++){
+            comboBox1.addItem(i);
+        }
+
+        comboBox1.addItemListener(new ItemChangeListener());
 
         GroupLayout contentPaneLayout = new GroupLayout(contentPane);
         contentPane.setLayout(contentPaneLayout);
         contentPaneLayout.setHorizontalGroup(
             contentPaneLayout.createParallelGroup()
-                .addGap(0, 400, Short.MAX_VALUE)
+                .addGroup(contentPaneLayout.createSequentialGroup()
+                    .addGroup(contentPaneLayout.createParallelGroup()
+                        .addGroup(contentPaneLayout.createSequentialGroup()
+                            .addGap(34, 34, 34)
+                            .addGroup(contentPaneLayout.createParallelGroup()
+                                .addComponent(label1)
+                                .addComponent(label2)))
+                        .addGroup(contentPaneLayout.createSequentialGroup()
+                            .addGap(42, 42, 42)
+                            .addGroup(contentPaneLayout.createParallelGroup()
+                                .addComponent(QuestionValue)
+                                .addGroup(contentPaneLayout.createSequentialGroup()
+                                    .addComponent(Select)
+                                    .addGap(18, 18, 18)
+                                    .addComponent(comboBox1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))))
+                    .addContainerGap(285, Short.MAX_VALUE))
         );
         contentPaneLayout.setVerticalGroup(
             contentPaneLayout.createParallelGroup()
-                .addGap(0, 300, Short.MAX_VALUE)
+                .addGroup(contentPaneLayout.createSequentialGroup()
+                    .addGap(45, 45, 45)
+                    .addComponent(label1)
+                    .addGap(18, 18, 18)
+                    .addComponent(label2)
+                    .addGap(18, 18, 18)
+                    .addGroup(contentPaneLayout.createParallelGroup(GroupLayout.Alignment.TRAILING)
+                        .addComponent(comboBox1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                        .addComponent(Select))
+                    .addGap(18, 18, 18)
+                    .addComponent(QuestionValue)
+                    .addContainerGap(131, Short.MAX_VALUE))
         );
         pack();
         setLocationRelativeTo(getOwner());
         // JFormDesigner - End of component initialization  //GEN-END:initComponents
+        setVisible(true);
     }
 
     // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
     // Generated using JFormDesigner Evaluation license - Jekaterina Zenkevica
+    private JLabel label1;
+    private JLabel label2;
+    private JComboBox comboBox1;
+    private JLabel Select;
+    private JLabel QuestionValue;
     // JFormDesigner - End of variables declaration  //GEN-END:variables
 }
--- ct414/AssessmentSystem.java
+++ ct414/AssessmentSystem.java
@@ -27,9 +27,15 @@ public class AssessmentSystem extends JFrame {
     boolean unAuthorised = false;
     String unAuthorisedExceptionReason = "";
     Assessment currentAssessment;
+    ExamServer server;
+    int token;
 
     public AssessmentSystem(ExamServer server, int username, int token) {
         this.ID_NUMBER = username;
+
+        this.server = server;
+
+        this.token = token;
 
         try{
             assessmentList = server.getAvailableSummary(token,username);
@@ -67,7 +73,6 @@ public class AssessmentSystem extends JFrame {
     private void initComponents() {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
         // Generated using JFormDesigner Evaluation license - Jekaterina Zenkevica
-        label1 = new JLabel();
         label2 = new JLabel();
         label3 = new JLabel();
         label4 = new JLabel();
@@ -83,9 +88,6 @@ public class AssessmentSystem extends JFrame {
         setTitle("Assessment System");
         Container contentPane = getContentPane();
 
-        //---- label1 ----
-        label1.setFont(label1.getFont().deriveFont(label1.getFont().getStyle() | Font.ITALIC, label1.getFont().getSize() + 10f));
-
         //---- label2 ----
         label2.setText("Student ID: " + ID_NUMBER);
         label2.setFont(label2.getFont().deriveFont(Font.BOLD|Font.ITALIC, label2.getFont().getSize() + 6f));
@@ -97,84 +99,90 @@ public class AssessmentSystem extends JFrame {
         //---- label4 ----
         label4.setText(unAuthorisedExceptionReason);
         label4.setVisible(unAuthorised);
+        if(!AssessmentNotAvailable) {
 
-        //======== panel1 ========
-        {
-            //---- label5 ----
-            label5.setText("Assignments Available: " + assessmentList.size());
+            //======== panel1 ========
+            {
+                //---- label5 ----
+                label5.setText("Assignments Available: " + assessmentList.size());
 
-            //---- label6 ----
-            label6.setText("Select from: ");
+                //---- label6 ----
+                label6.setText("Select from: ");
 
-            for(int i = 0; i<assessmentList.size(); i++){
-                comboBox1.addItem(assessmentList.get(i).getInformation());
+                for(int i = 0; i<assessmentList.size(); i++){
+                    comboBox1.addItem(assessmentList.get(i).getInformation());
+                }
+
+                currentAssessment = assessmentList.get(0);
+
+                comboBox1.addItemListener(new ItemChangeListener());
+
+                //---- label7 ----
+                label7.setText(currentAssessment.getInformation());
+
+                //---- label8 ----
+                label8.setText("Due: " + currentAssessment.getClosingDate());
+
+                //---- button1 ----
+                button1.setText("Start Assignment");
+                button1.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        setVisible(false);
+                        AssessmentGUI assessmentGUI = new AssessmentGUI(server,token,ID_NUMBER, currentAssessment);
+                    }
+                });
+
+                GroupLayout panel1Layout = new GroupLayout(panel1);
+                panel1.setLayout(panel1Layout);
+                panel1Layout.setHorizontalGroup(
+                        panel1Layout.createParallelGroup()
+                                .addGroup(panel1Layout.createSequentialGroup()
+                                        .addGap(14, 14, 14)
+                                        .addGroup(panel1Layout.createParallelGroup()
+                                                .addGroup(panel1Layout.createSequentialGroup()
+                                                        .addComponent(label5)
+                                                        .addContainerGap())
+                                                .addGroup(panel1Layout.createSequentialGroup()
+                                                        .addGroup(panel1Layout.createParallelGroup()
+                                                                .addComponent(label6)
+                                                                .addComponent(label7))
+                                                        .addGroup(panel1Layout.createParallelGroup()
+                                                                .addGroup(panel1Layout.createSequentialGroup()
+                                                                        .addGap(18, 18, 18)
+                                                                        .addComponent(comboBox1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                                                                        .addGap(0, 0, Short.MAX_VALUE))
+                                                                .addGroup(panel1Layout.createSequentialGroup()
+                                                                        .addGap(142, 142, 142)
+                                                                        .addComponent(label8)
+                                                                        .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 415, Short.MAX_VALUE)
+                                                                        .addComponent(button1)
+                                                                        .addGap(69, 69, 69))))))
+                );
+                panel1Layout.setVerticalGroup(
+                        panel1Layout.createParallelGroup()
+                                .addGroup(panel1Layout.createSequentialGroup()
+                                        .addContainerGap()
+                                        .addComponent(label5)
+                                        .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                                        .addGroup(panel1Layout.createParallelGroup()
+                                                .addComponent(comboBox1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                                                .addComponent(label6))
+                                        .addGroup(panel1Layout.createParallelGroup()
+                                                .addGroup(panel1Layout.createSequentialGroup()
+                                                        .addGap(32, 32, 32)
+                                                        .addGroup(panel1Layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                                                                .addComponent(label7)
+                                                                .addComponent(label8))
+                                                        .addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                                                .addGroup(GroupLayout.Alignment.TRAILING, panel1Layout.createSequentialGroup()
+                                                        .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                                        .addComponent(button1)
+                                                        .addGap(17, 17, 17))))
+                );
             }
 
-            comboBox1.addItemListener(new ItemChangeListener());
-
-            currentAssessment = assessmentList.get(0);
-
-            //---- label7 ----
-            label7.setText(currentAssessment.getInformation());
-
-            //---- label8 ----
-            label8.setText("Due: " + currentAssessment.getClosingDate());
-
-            //---- button1 ----
-            button1.setText("Start Assignment");
-
-            //---- button1 ----
-            button1.setText("Start Assignment");
-
-            GroupLayout panel1Layout = new GroupLayout(panel1);
-            panel1.setLayout(panel1Layout);
-            panel1Layout.setHorizontalGroup(
-                panel1Layout.createParallelGroup()
-                    .addGroup(panel1Layout.createSequentialGroup()
-                        .addGap(14, 14, 14)
-                        .addGroup(panel1Layout.createParallelGroup()
-                            .addGroup(panel1Layout.createSequentialGroup()
-                                .addComponent(label5)
-                                .addContainerGap())
-                            .addGroup(panel1Layout.createSequentialGroup()
-                                .addGroup(panel1Layout.createParallelGroup()
-                                    .addComponent(label6)
-                                    .addComponent(label7))
-                                .addGroup(panel1Layout.createParallelGroup()
-                                    .addGroup(panel1Layout.createSequentialGroup()
-                                        .addGap(18, 18, 18)
-                                        .addComponent(comboBox1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-                                        .addGap(0, 0, Short.MAX_VALUE))
-                                    .addGroup(panel1Layout.createSequentialGroup()
-                                        .addGap(142, 142, 142)
-                                        .addComponent(label8)
-                                        .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 415, Short.MAX_VALUE)
-                                        .addComponent(button1)
-                                        .addGap(69, 69, 69))))))
-            );
-            panel1Layout.setVerticalGroup(
-                panel1Layout.createParallelGroup()
-                    .addGroup(panel1Layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(label5)
-                        .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-                        .addGroup(panel1Layout.createParallelGroup()
-                            .addComponent(comboBox1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-                            .addComponent(label6))
-                        .addGroup(panel1Layout.createParallelGroup()
-                            .addGroup(panel1Layout.createSequentialGroup()
-                                .addGap(32, 32, 32)
-                                .addGroup(panel1Layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-                                    .addComponent(label7)
-                                    .addComponent(label8))
-                                .addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                            .addGroup(GroupLayout.Alignment.TRAILING, panel1Layout.createSequentialGroup()
-                                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                .addComponent(button1)
-                                .addGap(17, 17, 17))))
-            );
         }
-
         GroupLayout contentPaneLayout = new GroupLayout(contentPane);
         contentPane.setLayout(contentPaneLayout);
         contentPaneLayout.setHorizontalGroup(
@@ -185,9 +193,6 @@ public class AssessmentSystem extends JFrame {
                     .addGap(48, 48, 48))
                 .addGroup(contentPaneLayout.createSequentialGroup()
                     .addGroup(contentPaneLayout.createParallelGroup()
-                        .addGroup(contentPaneLayout.createSequentialGroup()
-                            .addGap(271, 271, 271)
-                            .addComponent(label1))
                         .addGroup(contentPaneLayout.createSequentialGroup()
                             .addGap(308, 308, 308)
                             .addComponent(label4)
@@ -201,9 +206,7 @@ public class AssessmentSystem extends JFrame {
         contentPaneLayout.setVerticalGroup(
             contentPaneLayout.createParallelGroup()
                 .addGroup(GroupLayout.Alignment.TRAILING, contentPaneLayout.createSequentialGroup()
-                    .addGap(24, 24, 24)
-                    .addComponent(label1)
-                    .addGap(28, 28, 28)
+                    .addGap(52, 52, 52)
                     .addComponent(label2)
                     .addGap(34, 34, 34)
                     .addComponent(panel1, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
@@ -221,7 +224,6 @@ public class AssessmentSystem extends JFrame {
 
     // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
     // Generated using JFormDesigner Evaluation license - Jekaterina Zenkevica
-    private JLabel label1;
     private JLabel label2;
     private JLabel label3;
     private JLabel label4;
